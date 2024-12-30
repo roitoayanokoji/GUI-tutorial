423ba7142484f7e3d8a9b5678715ed17aebd0b69
ayanokoji <roitoayanokoji@gmail.com>
2024-12-30 21:42:42 +0900

    commit

--- src/main/java/com/github/roitoayanokoji/gUITutorial/GUITutorial.java
+++ src/main/java/com/github/roitoayanokoji/gUITutorial/GUITutorial.java
@@ -1,5 +1,10 @@
 package com.github.roitoayanokoji.gUITutorial;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GUITutorial extends JavaPlugin {
@@ -15,5 +20,22 @@ public final class GUITutorial extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("OnDisable");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String args[]){
+        if (label.equalsIgnoreCase("gui")){
+            if (sender instanceof Player){
+                Player player  = (Player) sender;
+                openGUI(player);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void openGUI(Player player){
+        Inventory gui = Bukkit.createInventory(null,27,"GUI-Tutorial");
+        player.openInventory(gui);
     }
 }
